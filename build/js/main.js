9d9da8e5b1097006dd962ae38e0b61100bd8b4c5
Shiasu <vovan96mir@mail.ru>
2020-02-18 00:11:46 +0300

    Correct many issues in HTML/CSS, added new features. About 75% is ready

--- build/js/main.js
+++ build/js/main.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
-
-	$(".side-feature").click(function() {
-		$(this).toggleClass("spoiler-side-feature");
-		$(this).children("p").slideToggle(470);
-	});
-
+	//toggle(show/hide) full text of story
 	$(".story").click(function() {
 		$(this).toggleClass("spoiler-story");
-		$(this).children(".full-story").slideToggle(470);
+		$(this).children(".full-story").slideToggle();
+	});
+	//toggle(show/hide) full text of side article
+	$(".side-feature").click(function() {
+		$(this).toggleClass("spoiler-side-feature");
+		$(this).children("p").slideToggle();
 	});
 });
