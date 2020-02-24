e131d09c866d531e46bfa01a436e8a65d6fe9a02
Shiasu <vovan96mir@mail.ru>
2020-02-25 00:37:11 +0300

    Show/hide comments feature modified, adding statistics(only css/html)

--- build/js/main.js
+++ build/js/main.js
@@ -1,12 +1,41 @@
 $(document).ready(function(){
 	//toggle(show/hide) full text of story
-	$(".story").click(function() {
-		$(this).toggleClass("spoiler-story");
-		$(this).children(".full-story").slideToggle();
+	$(".story").click(function(event) {
+		let target = event.target;
+		if (target.className == "show-comments-button" || target.className == "add-comment-button") {
+			event.stopPropagation();
+		} else {
+			$(this).children(".full-story").slideToggle();
+		}
 	});
+
 	//toggle(show/hide) full text of side article
 	$(".side-feature").click(function() {
-		$(this).toggleClass("spoiler-side-feature");
 		$(this).children("p").slideToggle();
 	});
+
+	//Comment showing/hiding imitation on JS
+	$(".show-comments-button").click(function() {
+		let commentsDisplay = $(".story-comments").css("display");
+		if (commentsDisplay != "none")  {
+			$(".story-comments").css("display", "none");
+			$(this).html("Show comments");
+		} else {
+			$(".story-comments").css("display", "block");
+			$(this).html("Hide comments");
+
+			//Making random (name, text) and not random(date) data to comment
+			$(".comment-date").html(Date('2020-02-22T03:24:00'));
+
+			let names = ["Nick", "Paul", "John", "Mila", "Sofia", "Elena", "George", "Denise", "Kevin", "Ron"];
+			let secondNames = ["Rabbit", "Bear", "Seal", "Tiger", "Dog", "Cat", "Fox", "Deer", "Orca", "Raven"];
+			function getRandom() {
+				return Math.floor(Math.random() * 10);
+			}
+			let commentatorName = names[getRandom()];
+			let commentatorSecondName = secondNames[getRandom()];
+			$(".commentator-name").html(`${commentatorName} ${commentatorSecondName}`);
+			$(".comment-text").html(`Hello! My name is ${commentatorName}. My friend ${names[getRandom()]} show me this story today. It was exciting, because my ${secondNames[getRandom()]} had started speaking English arter that.`);
+		}
+	})
 });
