be69936594bc706f0c16891c98d63b97f9831ff6
Shiasu <vovan96mir@mail.ru>
2020-03-16 00:47:50 +0300

    Comment adding feature updated

--- build/js/main.js
+++ build/js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	//toggle(show/hide) full text of story
 	$(".story").click(function(event) {
 		let target = event.target;
-		if (target.className == "show-comments-button" || target.className == "add-comment-button") {
+		if (["comment-adding-name","show-comments-button", "add-comment-form-button", "comment-adding-text", "submit-comment-button"].indexOf(target.className) != -1) {
 			event.stopPropagation();
 		} else {
 			$(this).children(".full-story").slideToggle();
@@ -18,10 +18,10 @@ $(document).ready(function(){
 	$(".show-comments-button").click(function() {
 		let commentsDisplay = $(this).parent().next().css("display");
 		if (commentsDisplay != "none")  {
-			$(this).parent().next().css("display", "none");
+			$(this).parent().next(".story-comments").css("display", "none");
 			$(this).html("Show comments");
 		} else {
-			$(this).parent().next().css("display", "block");
+			$(this).parent().next(".story-comments").css("display", "block");
 			$(this).html("Hide comments");
 
 			//Making random (name, text) and not random(date) data to comment
@@ -36,6 +36,24 @@ $(document).ready(function(){
 			let commentatorSecondName = secondNames[getRandom()];
 			$(this).parent().next().find(".commentator-name").html(`${commentatorName} ${commentatorSecondName}`);
 			$(this).parent().next().find(".comment-text").html(`Hello! My name is ${commentatorName}. My friend ${names[getRandom()]} show me this story today. It was exciting, because my ${secondNames[getRandom()]} had started speaking English arter that.`);
+		}
+	});
+
+	//Comment adding imitation on JS
+	$(".add-comment-form-button").click(function() {
+		if ($(this).html() == "Add comment") {
+			$(this).parent().after().append("<div class='comment-input'><input class='comment-adding-name' type='text' placeholder='Enter your name'><textarea class='comment-adding-text' maxlength='150' placeholder='Comment text'></textarea><input class='submit-comment-button' type='submit' value='Send Comment'></div>");
+			$(this).html("Cancel");
+		} else {
+			$(this).parent().find(".comment-input").remove();
+			$(this).html("Add comment");
+		}
+	});
+
+	$(".submit-comment-button").click(function() {
+		console.log("text");
+		if ($(this).parent(".comment-input").find(".comment-adding-text").html()) {
+			$(this).parent(".full-story").siblings(".story-comments").before().append("<div class='comment'> <h3 class='commentator'Comment by></h3> <span class='commentator-name'></span> <p class='comment-date'></p><p class='comment-text'></p></div>");
 		}
 	});
 
